a9b2bcf1716c055486cf07be0552fb6f83d4cae4
barbosso <58640181+barbosso@users.noreply.github.com>
2020-07-30 17:51:07 +0300

    Add files via upload

---
+++ ds_bot.py
@@ -0,0 +1,34 @@
+import discord
+from discord.ext import commands
+import os
+from random import choice
+
+client = commands.Bot(command_prefix="!")
+
+client.remove_command("help")
+
+
+@client.command()
+@commands.has_permissions(administrator= True)
+async def load(ctx, extension):
+    client.load_extension(f"cogs.{extension}")
+
+
+@client.command()
+@commands.has_permissions(administrator= True)
+async def unload(ctx, extension):
+    client.unload_extension(f"cogs.{extension}")
+
+@client.command()
+@commands.has_permissions(administrator= True)
+async def clear(ctx, amount: int):
+    await ctx.channel.purge(limit=amount)
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f"cogs.{filename[:-3]}")
+
+
+token = os.environ.get('BOT_TOKEN')
+
+client.run(str(token))
